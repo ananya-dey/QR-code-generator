c52a42b635d335cdca3515e5f0c3b0d634c8bb1b
Ananya Dey <ananyadey007@gmail.com>
2017-03-14 01:08:57 +0600

    secon commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ new WOW().init();
 
 function generateQRCode() {
     this.QRimage.style.display = 'none';
+    if(encodeURIComponent(QRcode.value.trim()) == ''){
+        this.QRimage.style.display = 'none';
+    }
     this.QRimage.src = "https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=300x300&chl="
         + encodeURIComponent(QRcode.value.trim());
     this.QRimage.style.display = 'inline';
 }
+
+/*$(document).ready(function() {
+    $('.mdb-select').material_select();
+  });*/
